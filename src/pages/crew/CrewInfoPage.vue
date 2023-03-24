d1aaf0e944adb9ef19dd8f7bae7ffd01cca91711
sojung lee <sojong1003@gmail.com>
2023-03-24 20:05:46 +0900

    FEAT : 헤더에 내정보 버튼 추가

---
+++ src/pages/crew/CrewInfoPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <CrewInfo />
+</template>
+
+<script>
+import CrewInfo from '@/components/common/modify/CrewInfo.vue'
+export default {
+  components: {
+    CrewInfo,
+  },
+}
+</script>
+
+<style>
+</style>
