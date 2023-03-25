c0f128e0ddc50a225df725da465fe9c479ada3b2
Sayh0 <michaelyoon0948@gmail.com>
2023-03-26 04:22:15 +0900

    FEAT : ReservationList progress Bar 구현

--- src/components/mainPage/ReservationList.vue
+++ src/components/mainPage/ReservationList.vue
@@ -8,24 +8,22 @@
       <div class="card-body">
         <!--v-for-->
         <div
-          v-for="(reservation, index) in reservationList.slice(0, 5)"
-          :key="reservation.reservationId"
           class="reservations"
+          v-for="(reservation) in reservations.slice(0, 5)"
+          :key="reservation.reservationId"
         >
-          <!-- key 는 필수 -->
-          <h4 class="small fon--bold">
-            <span>No. {{ reservation.reservationId }}</span>
-            예약날짜 :{{ reservation.reservationDate | formatDate }}
-            <span class="float-right"></span>
+          <h4 class="small font-weight-bold">
+            <span>No. {{ reservation.reservationId }}</span> 예약날짜 :
+            {{ formattedReservationDate(reservation.reservationDate) }}
             <span class="float-right">{{ reservation.reservationStatus }}</span>
           </h4>
           <div class="progress progress-sm mb-4">
             <div
-              class="progress-bar"
-              v-bind:class="[`${reservation.reservationStatus}`]"
+              :class="progressBarColor(reservation.reservationStatus)"
               role="progressbar"
-              v-bind:id="[`${index}`]"
-              v-bind:style="{ width: '10%' }"
+              :style="{
+                width: progressBarWidth(reservation.reservationStatus),
+              }"
             ></div>
           </div>
         </div>
@@ -44,8 +42,55 @@ export default {
   },
   data() {
     return {
-      reservations: [], // 예약 리스트 데이터 배열
+      reservations: [],
     }
+  },
+  created() {
+    this.reservations = this.reservationList
+  },
+  watch: {
+    reservationList(newVal) {
+      this.reservations = newVal
+    },
+  },
+  methods: {
+    formattedReservationDate(reservationDate) {
+      return new Date(reservationDate).toLocaleString('ko-KR', {
+        timeZone: 'Asia/Seoul',
+        hour12: false,
+      })
+    },
+    progressBarWidth(reservationStatus) {
+      if (reservationStatus === '예약확정') {
+        return '20%'
+      } else if (reservationStatus === '검수완료') {
+        return '40%'
+      } else if (reservationStatus === '결제완료') {
+        return '50%'
+      } else if (reservationStatus === '세탁완료') {
+        return '75%'
+      } else if (reservationStatus === '배달완료') {
+        return '100%'
+      } else if (reservationStatus === '예약취소') {
+        return '100%'
+      } else {
+        return '10%'
+      }
+    },
+
+    progressBarColor(reservationStatus) {
+      if (reservationStatus === '결제완료') {
+        return 'progress-bar bg-info'
+      } else if (reservationStatus === '세탁완료') {
+        return 'progress-bar bg-info'
+      } else if (reservationStatus === '배달완료') {
+        return 'progress-bar bg-success'
+      } else if (reservationStatus === '예약취소') {
+        return 'progress-bar bg-dark'
+      } else {
+        return 'progress-bar bg-primary'
+      }
+    },
   },
 }
 </script>
