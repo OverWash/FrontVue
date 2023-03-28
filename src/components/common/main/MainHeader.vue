e390f0fe9de0a9ccbe61158627d9ac872d34b3d6
백성준 <qortjdwns120712@gmail.com>
2023-03-28 14:09:57 +0900

    FEAT : 내정보 토글 처리

--- src/components/common/main/MainHeader.vue
+++ src/components/common/main/MainHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="header">
     <div class="ms-4 row1">OverWash</div>
-
-    <div class="contain-item">
+<!--    v-show='role!=="ROLE_ADMIN"'-->
+    <div class="contain-item" >
       <span class="my-profile" @mouseover="mouseOn(true)"
             @mouseleave='mouseOn(false)'
         >내 정보
-        <MyProfileMenu v-show="menu" :class='menu ? "d-block": null '/>
+        <MyProfileMenu v-show="menu" :class='menu ? "d-block": null ' />
       </span>
       <span
         class="change-mode"
@@ -21,6 +21,7 @@
 <script>
 import { ref } from 'vue'
 import MyProfileMenu from '../MyProfileMenu.vue'
+import store from '@/store/store'
 
 export default {
   components: {
@@ -32,6 +33,7 @@ export default {
       content: '다크모드',
     })
     const menu = ref(false)
+    const role = ref(store.state.role)
 
     const darkModeBtn = () => {
       if (theme.value.theme === 'light') {
@@ -55,7 +57,8 @@ export default {
       theme,
       darkModeBtn,
       mouseOn,
-      menu
+      menu,
+      role
     }
   },
 }
